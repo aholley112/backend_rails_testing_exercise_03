0107f1d64dd8c581a9d743e1361010dec199d041
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-09 13:15:39 -0600

    exercise_03

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /posts" do
+    before do
+      create_list(:post, 3)
+      get '/posts'
+    end
+    it "returns all posts" do
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
   end
 end
